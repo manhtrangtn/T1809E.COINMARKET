8d6ad7db8ab3758247be9056cc74cd46d2c7c72e
manhtrangtn <manhtrangtn@gmail.com>
2020-06-02 13:06:01 +0700

    Created cryptoCompare get coin information

--- T1809E.COINMARKET/Controllers/HomeController.cs
+++ T1809E.COINMARKET/Controllers/HomeController.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using T1809E.COINMARKET.Respositories;
+using T1809E.COINMARKET.Services;
 
 namespace T1809E.COINMARKET.Controllers
 {
     public class HomeController : Controller
     {
+      private CryptoCompareService _cryptoCompareService = new CryptoCompareService();
+      private CloudFirestoreRepository _cloudFirestoreRepository = new CloudFirestoreRepository();
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
-
+            _cryptoCompareService.CryptoCompareWebSocket();
+            _cloudFirestoreRepository.GetConnection();
             return View();
         }
     }
---
+++ T1809E.COINMARKET/Models/Coins/CryptoCompareCoin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace T1809E.COINMARKET.Models.Coins
+{
+    public class CryptoCompareCoin
+    {
+      public string Symbol { get; set; }
+      public string Timestamp { get; set; }
+      public string CurrencySymbol { get; set; }
+      public double Price { get; set; }
+    }
+}
