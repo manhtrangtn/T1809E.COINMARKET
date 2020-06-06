dfeba02726a1425cd1be0294b0b241dec94c0daf
manhtrangtn <manhtrangtn@gmail.com>
2020-06-06 22:25:00 +0700

    latest codes

---
+++ T1809E.COINMARKET/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using T1809E.COINMARKET.Models;
+
+namespace T1809E.COINMARKET.Controllers
+{
+    public class AdminController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            var posts = db.Posts.Include(p => p.Rank).Include(p => p.User);
+            return View(posts.ToList());
+        }
+    }
+}
--- T1809E.COINMARKET/Controllers/HomeController.cs
+++ T1809E.COINMARKET/Controllers/HomeController.cs
@@ -5,17 +5,12 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Newtonsoft.Json;
-using T1809E.COINMARKET.Respositories;
-using T1809E.COINMARKET.Services;
 using WebSocketSharp;
 
 namespace T1809E.COINMARKET.Controllers
 { 
   public class HomeController : Controller
     {
-      private readonly CryptoCompareService _cryptoCompareService = new CryptoCompareService();
-      private BinanceService _binanceService = new BinanceService();
-      private CloudFirestoreRepository _cloudFirestoreRepository = new CloudFirestoreRepository();
       public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
